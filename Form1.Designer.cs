26a3c86c9bfe9898098e20867f4187c144b1aa50
Anastasiia000 <57679919+Anastasiia000@users.noreply.github.com>
2020-10-05 10:31:42 +0300

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Pingpong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.background = new System.Windows.Forms.Panel();
+            this.gamePanel = new System.Windows.Forms.PictureBox();
+            this.gameBall = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.background.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gamePanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // background
+            // 
+            this.background.Controls.Add(this.gameBall);
+            this.background.Controls.Add(this.gamePanel);
+            this.background.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(800, 450);
+            this.background.TabIndex = 0;
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.Color.Yellow;
+            this.gamePanel.Location = new System.Drawing.Point(47, 395);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(670, 16);
+            this.gamePanel.TabIndex = 0;
+            this.gamePanel.TabStop = false;
+            // 
+            // gameBall
+            // 
+            this.gameBall.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.gameBall.Location = new System.Drawing.Point(160, 103);
+            this.gameBall.Name = "gameBall";
+            this.gameBall.Size = new System.Drawing.Size(50, 50);
+            this.gameBall.TabIndex = 1;
+            this.gameBall.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.background.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gamePanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel background;
+        private System.Windows.Forms.PictureBox gameBall;
+        private System.Windows.Forms.PictureBox gamePanel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pingpong
+{
+    public partial class Form1 : Form
+    {
+        private int speedVertical = 2, speedHorizon = 2, score=0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            gamePanel.Left = Cursor.Position.X - (gamePanel.Width / 2);
+            gameBall.Left += speedHorizon;
+            gameBall.Top += speedVertical;
+            if (gameBall.Left <= background.Left)
+                speedHorizon *= -1;
+            if (gameBall.Right >= background.Right)
+                speedHorizon *= -1;
+            if (gameBall.Top <= background.Top)
+                speedVertical *= -1;
+            if (gameBall.Bottom >= background.Bottom)
+                timer1.Enabled=false;
+            if (gameBall.Bottom >= gamePanel.Top && gameBall.Bottom <= gamePanel.Bottom
+                && gameBall.Left >= gamePanel.Left && gameBall.Right <= gamePanel.Right) 
+            {
+                speedVertical += 1;
+                speedHorizon += 1;
+                speedVertical *= -1;
+                score += 1;
+                if (gamePanel.Width >= 80)
+                    gamePanel.Width -= 35;
+                else
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("You winner!! Your score: " + score);
+                    Close();
+                }
+
+            } 
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true; //іконки програми будуть рухатися
+            Cursor.Hide(); //ховаємо курсор
+            this.FormBorderStyle = FormBorderStyle.None; //безграниць стиль програми
+            this.TopMost = true; //наша програма запуститься поверху інших програм
+            this.Bounds = Screen.PrimaryScreen.Bounds; //розміри нашої програми буде по висоті і ширині нашого екрану
+            gamePanel.Top = background.Bottom - (background.Bottom / 10); //наша панелька, яка відбиває мячік буде прижата до нижнього краю, завжди на певній відстанні
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+
+        }
+    }
+}
